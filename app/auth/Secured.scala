c93969da5933f884aa43cebbfcb5f6a769f90bf9
Luigi Antonini <luigi.antonini84@gmail.com>
2013-09-08 12:24:53 +0200

    added scaladocs

--- app/auth/Secured.scala
+++ app/auth/Secured.scala
@@ -24,7 +24,7 @@ trait Secured extends BodyParsers with Loggable {
   def secureUsersRetriever: SecureUsersRetriever
 
   /**
-   * Try to retrive the username of the user,
+   * Try to retrieve the username of the user,
    * before from the standard play session,
    * and after from a the remember me cookie
    *
@@ -51,7 +51,7 @@ trait Secured extends BodyParsers with Loggable {
 
   /**
    * This function is called when user is not authorized,
-   * because probably we want a redirect, we leave the controller to imlpement that
+   * because probably we want a redirect, we leave the controller to implement that
    * function
    *
    * @param request
@@ -59,11 +59,26 @@ trait Secured extends BodyParsers with Loggable {
    */
   def onUnauthorized(request: RequestHeader): Result
 
+  /**
+   * This function is called when a rest call is not authorized,
+   * a default message can be provided in that case
+   *
+   * @param request
+   * @return
+   */
   def onUnauthorizedRest(request: RequestHeader): Result = {
     log.debug(s"on onUnauthorized ip : ${request.remoteAddress}")
     Results.Ok("not authorized")
   }
 
+  /**
+   * Create a WebSocket loading the username,
+   * if user not exist create a WebSocket which send the not
+   * authorized message and close the connection
+   *
+   * @param f
+   * @return
+   */
   def withAuthWS(f: => Int => Future[(Iteratee[JsValue, Unit], Enumerator[JsValue])]): WebSocket[JsValue] = {
 
     def errorFuture = {
@@ -96,6 +111,16 @@ trait Secured extends BodyParsers with Loggable {
 
   }
 
+  /**
+   * The scope of this function is to wrap the creation of
+   * an acton, providing the username of the user,
+   * retrieved from the session or from the remember cookie
+   *
+   * @param f
+   * @param authF
+   * @param parser
+   * @return
+   */
   def withAuthBase(f: => String => Request[_ >: AnyContent] => Result,
                    authF: RequestHeader => Result = onUnauthorized,
                    parser: BodyParser[_ >: AnyContent] = parse.anyContent): EssentialAction = {
@@ -107,6 +132,21 @@ trait Secured extends BodyParsers with Loggable {
 
   }
 
+  /**
+   * This function, relying on  withAuthBase,
+   * try to pass the user loaded from SecureUsersRetriever to the
+   * wrapped action
+   *
+   * Don't use this function directly,
+   * use on of the withUser implementation
+   *
+   * @param authF
+   * @param parser
+   * @param userFilter
+   * @param f
+   * @tparam T
+   * @return
+   */
   def withUserBase[T <: SecureUser](authF: RequestHeader => Result = onUnauthorized,
                                     parser: BodyParser[_ >: AnyContent] = parse.anyContent,
                                     userFilter: SecureUser => Boolean = _ => true)
@@ -122,28 +162,80 @@ trait Secured extends BodyParsers with Loggable {
 
   }
 
-
+  /**
+   * Default implementation, should be used to wrap
+   * html actions, call onUnauthorized implementation if
+   * user is not present
+   *
+   * @tparam T
+   * @return
+   */
   def withUser[T <: SecureUser]: ((T) => (Request[_ >: AnyContent]) => Result) => EssentialAction = withUserBase() _
 
+  /**
+   * Default implementation for rest calls,
+   * call onUnauthorizedRest if user not exists,
+   * to use for standard rest GET
+   *
+   * @tparam T
+   * @return
+   */
   def withRestUser[T <: SecureUser]: ((T) => (Request[_ >: AnyContent]) => Result) => EssentialAction = withUserBase(onUnauthorizedRest) _
 
+  /**
+   * This implementation must be used for rest calls,
+   * when the request have a JSON body
+   *
+   * @tparam T
+   * @return
+   */
   def withJsonUser[T <: SecureUser]: ((T) => (Request[_ >: AnyContent]) => Result) => EssentialAction = withUserBase(onUnauthorizedRest, parse.json) _
 
+  /**
+   * Same as withUser call,
+   * verify that user is admin
+   *
+   * @tparam T
+   * @return
+   */
   def withAdmin[T <: SecureUser]: ((T) => (Request[_ >: AnyContent]) => Result) => EssentialAction = withUserBase(userFilter = _.isAdmin) _
 
+
+  /**
+   * Same as withUser call,
+   * verify that user is admin
+   *
+   * @tparam T
+   * @return
+   */
   def withRestAdmin[T <: SecureUser]: ((T) => (Request[_ >: AnyContent]) => Result) => EssentialAction = withUserBase(onUnauthorizedRest, userFilter = _.isAdmin) _
 
+
+  /**
+   * Same as withUser call,
+   * verify that user is admin
+   *
+   * @tparam T
+   * @return
+   */
   def withJsonAdmin[T <: SecureUser]: ((T) => (Request[_ >: AnyContent]) => Result) => EssentialAction = withUserBase(onUnauthorizedRest, parse.json, userFilter = _.isAdmin) _
 
 }
 
-
+/**
+ * The DAO class, must mix that trait for allow
+ * secured trait to load user from database
+ *
+ */
 trait SecureUsersRetriever {
   def findByEmail(email: String): Option[SecureUser]
 
   def findByRemember(cookie: String): Option[SecureUser]
 }
 
+/**
+ * Mi that trait in User implementation
+ */
 trait SecureUser {
   def id: Option[Int]
 
